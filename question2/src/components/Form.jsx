4f0c54be59f53760a1921d9fa6986009c2417cfa
Tharsikan <thar.navaratnam@gmail.com>
2022-02-02 01:04:56 -0500

    final cleanup

--- question2/src/components/Form.jsx
+++ question2/src/components/Form.jsx
@@ -19,24 +19,24 @@ function Form() {
 
   return (
     <form>
-      <label for="full-name">Your full given name:</label>
+      <label htmlFor="full-name">Your full given name:</label>
       <input type="text" id="full-name" name="full-name" className="fullwidth"></input>
       <div className="side-by-side">
         <div className="input-wrapper left-input">
-          <label for="birthday">Date of Birth</label>
+          <label htmlFor="birthday">Date of Birth</label>
           <div className="icon-wrapper">
             <i className="fa fa-calendar" aria-hidden="true"></i>
             <input type="text" value={date} readOnly id="birthday" name="birthday"></input>
           </div>
         </div>
         <div className="input-wrapper right-input">
-          <label for="country">Country of residence or citizenship:</label>
+          <label htmlFor="country">Country of residence or citizenship:</label>
           <input type="text" id="country" name="country"></input>
         </div>
       </div>
-      <label for="school">What school do you plan to attend?</label>
+      <label htmlFor="school">What school do you plan to attend?</label>
       <input type="text" id="school" name="school" className="fullwidth"></input>
-      <label for="study-area">Please take a moment to describe your intended area of study:</label>
+      <label htmlFor="study-area">Please take a moment to describe your intended area of study:</label>
       <textarea id="study-area" className="fullwidth" name="study-area" placeholder="Enter details here" rows="6"></textarea>
     </form>
   )
