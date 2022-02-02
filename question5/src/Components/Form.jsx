509148f90c2f8acf3c060916d829f6c87d1d9510
Tharsikan <thar.navaratnam@gmail.com>
2022-02-02 11:07:33 -0500

    setup controlled input for word

--- question5/src/Components/Form.jsx
+++ question5/src/Components/Form.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, TextField } from "@mui/material";
 import "./form.css";
 
 function Form() {
+  const [word, setWord] = useState("");
+
+  const handleWord = (e) => {
+    setWord(e.target.value);
+  };
+
+  const handleSubmission = (e) => {
+    e.preventDefault();
+  };
 
   return (
-    <form>
-      <TextField sx={{ m: "1em" }} />
+    <form onSubmit={handleSubmission}>
+      <TextField sx={{ m: "1em" }} value={word} onChange={handleWord} />
       <Button variant="contained" type="submit">Find Words!</Button>
     </form>
   )
