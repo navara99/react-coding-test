951f9a9ab3d9cfa6ec10a4a9860900b3a827d2d5
Tharsikan <thar.navaratnam@gmail.com>
2022-02-02 00:41:38 -0500

    remove 0 if day is less than 10

--- question2/src/components/helper.js
+++ question2/src/components/helper.js
@@ -15,9 +15,13 @@ export const formatDate = (date) => {
   ];
 
   const dateArr = date.split("/");
-  const month = monthNames[Number(dateArr[0]) - 1]
-  const day = dateArr[1];
+  const month = monthNames[Number(dateArr[0]) - 1];
   const year = dateArr[2];
+  let day = dateArr[1];
+
+  if (day < 10) {
+    day = Number(day.toString().split("")[1]);
+  };
 
   return `${month} ${day}, ${year}`;
 };
