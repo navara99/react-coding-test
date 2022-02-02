28e5128ff58dd3327d6e8cc69d3aa2ec4b0350f8
Tharsikan <thar.navaratnam@gmail.com>
2022-02-02 12:26:42 -0500

    refactor with switch statement

--- question3/question3.js
+++ question3/question3.js
@@ -19,5 +19,26 @@
 
 /********************* Refactored Code *********************/
 
+// In this refactor amt = base * fudge is repeated, normally I would move this into its own small
+// arrow function but as per instruction I did not make new variables or functions.
 
+switch (recipe) {
+  case "SPANISH":
+    fudge = SPANISH_FUDGE;
+    amt = base * fudge;
+    break;
+  case "FRENCH":
+    fudge = FRENCH_FUDGE;
+    amt = base * fudge;
+    chocolate = 7;
+    break;
+  case "ENGLISH":
+    fudge = ENGLISH_FUDGE;
+    amt = base * fudge;
+    break;
+  default:
+    fudge = 1;
+    amt = base;
+};
 
+sugar = 2 * bottom(amt) + top(amt) * 1.17;
