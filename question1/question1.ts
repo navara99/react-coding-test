ea7be44c18cd40ce3cc8ea573c598195098da6fa
Tharsikan <thar.navaratnam@gmail.com>
2022-02-02 12:53:41 -0500

    implement working trivial solution

---
+++ question1/question1.ts
@@ -0,0 +1,13 @@
+const candyBar = (num: number) => {
+
+  if (num % 22 === 0) {
+    console.log("candybar");
+  } else if (num % 11 === 0) {
+    console.log("bar")
+  } else if (num % 2 === 0) {
+    console.log("candy");
+  } else {
+    console.log(num);
+  };
+
+};
